37204a9e3d9bff58320c095cb33eef86d2e136c8
abubdcalling <abubdcalling@gmail.com>
2025-06-03 21:08:10 +0600

    add

--- database/migrations/2025_06_03_144513_create_sub_categories_table.php
+++ database/migrations/2025_06_03_144513_create_sub_categories_table.php
@@ -13,9 +13,13 @@ return new class extends Migration
     {
         Schema::create('sub_categories', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->unsignedBigInteger('category_id');
             $table->string('name');
             $table->timestamps();
+
+            $table->foreign('category_id')
+                ->references('id')->on('categories')
+                ->onDelete('cascade');
         });
     }
 
