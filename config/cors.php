50c0972bddc4e7f311868b1fad570ed3a903bb3a
abubdcalling <abubdcalling@gmail.com>
2025-06-03 23:24:10 +0600

    ADD

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths' => ['api/*'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 
     'allowed_origins' => ['http://localhost:3000'],
 
